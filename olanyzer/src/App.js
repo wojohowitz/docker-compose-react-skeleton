6d494b669591c930693790c9ecc33d152a1a5602
Wojtek Augustynski <wojtek@Wojteks-MacBook-Air.local>
2017-07-03 13:04:42 -0700

    renaming

--- olanyzer/src/App.js
+++ olanyzer/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Container, Header } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to My House!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container text>
+        <Header as='h2'>Header</Header>
+        <p>Woot</p>
+        <p>Woot</p>
+        <p>Woot</p>
+        <p>Woot</p>
+      </Container>
     );
   }
 }
